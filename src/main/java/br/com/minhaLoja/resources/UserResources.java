228ab97ccd3fa49a2ccf9dd9d8fd0bfb09cc6f8c
geversonfernandes <gever.fernandes@gmail.com>
2023-09-22 10:37:49 -0300

    user entity and resource

---
+++ src/main/java/br/com/minhaLoja/resources/UserResources.java
@@ -0,0 +1,18 @@
+package br.com.minhaLoja.resources;
+
+import br.com.minhaLoja.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "99999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
