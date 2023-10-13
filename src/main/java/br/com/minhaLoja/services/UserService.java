c6c4a74f20e594e8fe1837beb80503029fdb9317
geversonfernandes <gever.fernandes@gmail.com>
2023-10-12 21:24:36 -0300

    exception handling - delete

--- src/main/java/br/com/minhaLoja/services/UserService.java
+++ src/main/java/br/com/minhaLoja/services/UserService.java
@@ -2,8 +2,11 @@ package br.com.minhaLoja.services;
 
 import br.com.minhaLoja.entities.User;
 import br.com.minhaLoja.repositories.UserRepository;
+import br.com.minhaLoja.services.exceptions.DatabaseException;
 import br.com.minhaLoja.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
